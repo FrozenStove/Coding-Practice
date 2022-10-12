3dc0a12ae06d11b6f57d7e72e0de0658a0aa3763
FrozenStove <Yessysutter@gmail.com>
2022-10-12 11:04:52 -0700

    Powerset and TaskAssign

---
+++ powerset.js
@@ -0,0 +1,23 @@
+/* Powerset
+
+write a function that takes in an array of unique integers and returns its powerset.
+
+The powerset P(X) of a set X is the set of all subset of X. For example, 
+the powerset of [1,2] is [[], [1], [2], [1,2]].
+
+Note that the sets in the powerset do not need to be in any particular order
+
+SAMPLE INPUT
+
+array = [1,2,3]
+
+SAMPLE OUTPUT
+
+[[], [1], [2], [3], [1,2], [1,3], [2,3], [1,2,3]]
+
+
+*/
+
+function powerset(array) {
+    // Write your code here.
+}
