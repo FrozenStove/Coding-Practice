79af6ff8a0e96ef7eb546db8df7761414eb0ca0b
FrozenStove <Yessysutter@gmail.com>
2022-10-23 12:59:02 -0700

    Faster Solution Found

--- 01matrix.js
+++ 01matrix.js
@@ -17,23 +17,24 @@ var updateMatrix = function (mat) {
 
 
     const helper = (mat, x, y, currentPath = [], distance = 0) => {
-        
+
         if (currentPath.includes(x + ',' + y) || mat[x] === undefined || mat[x][y] === undefined) {
             return Infinity
             // I ran into errors because i was adding in 0 elements into the cache
             // there is another error where the shortest possible path to a zero is added into the cache too early
             // solution is fixed by passing in an array representing the current path as the cache
             // the array is different for each path
-        } else if(mat[x][y] !== 0){
+        } else if (mat[x][y] !== 0) {
             currentPath.push(x + ',' + y)
         }
-        
+
         // base case
         if (mat[x][y] === 0) {
             cache.pop()
             return distance
         }
-                const subarray = [
+
+        const subarray = [
             helper(mat, x, y + 1, [...currentPath], distance + 1,),
             helper(mat, x, y - 1, [...currentPath], distance + 1,),
             helper(mat, x - 1, y, [...currentPath], distance + 1,),
@@ -53,13 +54,48 @@ var updateMatrix = function (mat) {
     }
     return output
 };
-Input: mat = [[1, 1, 1], [1, 1, 0], [1, 1, 0]]
+
+// the previous method was overly complicated and not efficient due to the recursion
+// the solution below is much more efficient than above by just calcuating the distance to each zero from each one
+// still achieves an approximate O(n^2) time
+
+var updateMatrixFast = function (mat) {
+
+    const output = [];
+    const zeroArray = [];
+    const oneArray = [];
+    for (const element of mat) output.push(Array(mat[0].length).fill(0));
+
+    for (let m = 0; m < mat.length; m++) {
+        for (let n = 0; n < mat[0].length; n++) {
+            if (mat[m][n] !== 0) {
+                oneArray.push([m, n]);
+            } else {
+                zeroArray.push([m, n]);
+            }
+        }
+    }
+
+    for (let one of oneArray) {
+        let minDist = Infinity
+        for ( let zero of zeroArray){
+            let currDist = Math.abs(one[0] - zero[0]) + Math.abs(one[1] - zero[1])
+            minDist = currDist < minDist ? currDist:minDist
+        }
+        output[one[0]][one[1]] = minDist;
+    }
+    return output
+};
+
+// Input: mat = [[1, 1, 1], [1, 1, 0], [1, 1, 0]]
 
 // mat = [[1,0,1,1,0,0,1,0,0,1],[0,1,1,0,1,0,1,0,1,1],[0,0,1,0,1,0,0,1,0,0],[1,0,1,0,1,1,1,1,1,1],[0,1,0,1,1,0,0,0,0,1],[0,0,1,0,1,1,1,0,1,0],[0,1,0,1,0,1,0,0,1,1],[1,0,0,0,1,1,1,1,0,1],[1,1,1,1,1,1,1,0,1,0],[1,1,1,1,0,1,0,0,1,1]]
 
-// math = [[0,0,1,0,1,1,1,0,1,1],[1,1,1,1,0,1,1,1,1,1],[1,1,1,1,1,0,0,0,1,1],[1,0,1,0,1,1,1,0,1,1],[0,0,1,1,1,0,1,1,1,1],[1,0,1,1,1,1,1,1,1,1],[1,1,1,1,0,1,0,1,0,1],[0,1,0,0,0,1,0,0,1,1],[1,1,1,0,1,1,0,1,0,1],[1,0,1,1,1,0,1,1,1,0]]
+mat = [[0, 0, 1, 0, 1, 1, 1, 0, 1, 1], [1, 1, 1, 1, 0, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 0, 0, 0, 1, 1], [1, 0, 1, 0, 1, 1, 1, 0, 1, 1], [0, 0, 1, 1, 1, 0, 1, 1, 1, 1], [1, 0, 1, 1, 1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 0, 1, 0, 1, 0, 1], [0, 1, 0, 0, 0, 1, 0, 0, 1, 1], [1, 1, 1, 0, 1, 1, 0, 1, 0, 1], [1, 0, 1, 1, 1, 0, 1, 1, 1, 0]]
+// console.table(mat)
 console.time('start')
-console.table(updateMatrix(mat))
+// console.table(updateMatrix(mat))
+console.table(updateMatrixFast(mat))
 console.timeEnd('start')
 // console.log(mat[1][0])
 // console.log(mat[2][0])
