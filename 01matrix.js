9980c9414210bf3a9a02f06f0a921701f3295b5a
FrozenStove <Yessysutter@gmail.com>
2022-10-23 12:10:07 -0700

    too slow

--- 01matrix.js
+++ 01matrix.js
@@ -16,48 +16,55 @@ var updateMatrix = function (mat) {
     // let flag = 0;
 
 
-    const helper = (mat, x, y, distance = 0) => {
-
-        if (cache.has(x + ',' + y) || mat[x] === undefined || mat[x][y] === undefined) {
+    const helper = (mat, x, y, currentPath = [], distance = 0) => {
+        
+        if (currentPath.includes(x + ',' + y) || mat[x] === undefined || mat[x][y] === undefined) {
             return Infinity
             // I ran into errors because i was adding in 0 elements into the cache
             // there is another error where the shortest possible path to a zero is added into the cache too early
+            // solution is fixed by passing in an array representing the current path as the cache
+            // the array is different for each path
         } else if(mat[x][y] !== 0){
-            cache.add(x + ',' + y)
+            currentPath.push(x + ',' + y)
         }
-
-        // base case
-        if (mat[x][y] === 0) return distance
         
-        const subarry = [
-            helper(mat, x, y + 1, distance + 1),
-            helper(mat, x, y - 1, distance + 1),
-            helper(mat, x - 1, y, distance + 1),
-            helper(mat, x + 1, y, distance + 1),
+        // base case
+        if (mat[x][y] === 0) {
+            cache.pop()
+            return distance
+        }
+                const subarray = [
+            helper(mat, x, y + 1, [...currentPath], distance + 1,),
+            helper(mat, x, y - 1, [...currentPath], distance + 1,),
+            helper(mat, x - 1, y, [...currentPath], distance + 1,),
+            helper(mat, x + 1, y, [...currentPath], distance + 1,),
         ]
 
-        return Math.min(...subarry);
+        return Math.min(...subarray);
     }
 
     for (let m = 0; m < mat.length; m++) {
         for (let n = 0; n < mat[0].length; n++) {
             if (mat[m][n] !== 0) {
-                cache = new Set();
-                output[m][n] = helper(mat, m, n)
+                cache = [];
+                output[m][n] = helper(mat, m, n, cache)
             }
         }
     }
     return output
 };
-// Input: mat = [[1, 1, 1], [1, 1, 0], [1, 1, 0]]
+Input: mat = [[1, 1, 1], [1, 1, 0], [1, 1, 0]]
 
-mat = [[1,0,1,1,0,0,1,0,0,1],[0,1,1,0,1,0,1,0,1,1],[0,0,1,0,1,0,0,1,0,0],[1,0,1,0,1,1,1,1,1,1],[0,1,0,1,1,0,0,0,0,1],[0,0,1,0,1,1,1,0,1,0],[0,1,0,1,0,1,0,0,1,1],[1,0,0,0,1,1,1,1,0,1],[1,1,1,1,1,1,1,0,1,0],[1,1,1,1,0,1,0,0,1,1]]
+// mat = [[1,0,1,1,0,0,1,0,0,1],[0,1,1,0,1,0,1,0,1,1],[0,0,1,0,1,0,0,1,0,0],[1,0,1,0,1,1,1,1,1,1],[0,1,0,1,1,0,0,0,0,1],[0,0,1,0,1,1,1,0,1,0],[0,1,0,1,0,1,0,0,1,1],[1,0,0,0,1,1,1,1,0,1],[1,1,1,1,1,1,1,0,1,0],[1,1,1,1,0,1,0,0,1,1]]
 
+// math = [[0,0,1,0,1,1,1,0,1,1],[1,1,1,1,0,1,1,1,1,1],[1,1,1,1,1,0,0,0,1,1],[1,0,1,0,1,1,1,0,1,1],[0,0,1,1,1,0,1,1,1,1],[1,0,1,1,1,1,1,1,1,1],[1,1,1,1,0,1,0,1,0,1],[0,1,0,0,0,1,0,0,1,1],[1,1,1,0,1,1,0,1,0,1],[1,0,1,1,1,0,1,1,1,0]]
+console.time('start')
 console.table(updateMatrix(mat))
+console.timeEnd('start')
 // console.log(mat[1][0])
 // console.log(mat[2][0])
 
-console.table([[1,0,1,1,0,0,1,0,0,1],[0,1,1,0,1,0,1,0,1,1],[0,0,1,0,1,0,0,1,0,0],[1,0,1,0,1,1,1,1,1,1],[0,1,0,1,1,0,0,0,0,1],[0,0,1,0,1,1,1,0,1,0],[0,1,0,1,0,1,0,0,1,1],[1,0,0,0,1,2,1,1,0,1],[2,1,1,1,1,2,1,0,1,0],[3,2,2,1,0,1,0,0,1,1]])
+// console.table([[1,0,1,1,0,0,1,0,0,1],[0,1,1,0,1,0,1,0,1,1],[0,0,1,0,1,0,0,1,0,0],[1,0,1,0,1,1,1,1,1,1],[0,1,0,1,1,0,0,0,0,1],[0,0,1,0,1,1,1,0,1,0],[0,1,0,1,0,1,0,0,1,1],[1,0,0,0,1,2,1,1,0,1],[2,1,1,1,1,2,1,0,1,0],[3,2,2,1,0,1,0,0,1,1]])
 let m
 let n = 6
 // console.log('[',m+','+n,']')
