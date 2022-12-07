530ba5546f5829b02d3561adf1eafb5485e1c06a
FrozenStove <Yessysutter@gmail.com>
2022-12-07 14:40:56 -0800

    day7

---
+++ advent/day7.js
@@ -0,0 +1,11 @@
+const fs = require('fs')
+
+fs.readFile('/Users/lennysutrisno/Documents/GitHub/Coding-Practice/advent/day6_in.txt', (err, data) => {
+    if (err) throw err;
+
+    let inputString = data.toString();
+    let input = inputString.split("\n");
+
+    // let
+
+})
