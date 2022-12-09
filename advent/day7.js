81ac8449f45e9207dab16fafc587bf439ce6fb02
FrozenStove <Yessysutter@gmail.com>
2022-12-08 22:35:27 -0800

    Update day7.js

--- advent/day7.js
+++ advent/day7.js
@@ -1,5 +1,38 @@
 const fs = require('fs')
 
+class Directory {
+    constructor () {
+        this.cd = [];
+    }
+
+    currentDirectory() {
+
+    } 
+
+    add() {
+
+    }
+
+}
+
+class File {
+    constructor(){
+        // this.size:
+    }
+}
+
+const data = {
+    '/': {
+        a:{},
+        'b.txt':14,
+        'c.dat':85,
+    }
+}
+
+const thePath = new Directory()
+
+console.log(thePath.cd)
+
 fs.readFile('/Users/lennysutrisno/Documents/GitHub/Coding-Practice/advent/day6_in.txt', (err, data) => {
     if (err) throw err;
 
